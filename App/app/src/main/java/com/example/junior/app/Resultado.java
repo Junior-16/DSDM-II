5865c4f3d1e288e6ce8d434d4ae5b2a03197bc0c
Junior-16 <junior.ramisch@gmail.com>
2017-05-23 15:02:46 -0300

    Aula de DSDM II

---
+++ App/app/src/main/java/com/example/junior/app/Resultado.java
@@ -0,0 +1,31 @@
+package com.example.junior.app;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Resultado extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resultado);
+        TextView mediaview = (TextView) findViewById(R.id.media);
+        Intent notas = getIntent();
+        Bundle bd = notas.getExtras();
+        try {
+            double n1 = Double.parseDouble(bd.getString("nota1"));
+            double n2 = Double.parseDouble(bd.getString("nota2"));
+            double n3 = Double.parseDouble(bd.getString("nota3"));
+            int p1 = Integer.parseInt(bd.getString("peso1"));
+            int p2 = Integer.parseInt(bd.getString("peso2"));
+            int p3 = Integer.parseInt(bd.getString("peso3"));
+            double media1 = ((n1 * p1) + (n2 * p2) + (n3 * p3)) / (p1 + p2 + p3);
+            mediaview.setText("" + (media1));
+        }catch(java.lang.NumberFormatException e){
+            Toast.makeText(getApplicationContext(), "Nota ou peso não informado", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
---
+++ Notas/app/src/main/java/com/example/junior/notas/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.junior.notas;
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText nota1;
+    private EditText nota2;
+    private EditText nota3;
+    private EditText peso1;
+    private EditText peso2;
+    private EditText peso3;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+    public void enviar(View v){
+        Intent result = new Intent("com.example.junior.app.MEDIA");
+        nota1 = (EditText) findViewById(R.id.nota1);
+        nota2 = (EditText) findViewById(R.id.nota2);
+        nota3 = (EditText) findViewById(R.id.nota3);
+        peso1 = (EditText) findViewById(R.id.peso1);
+        peso2 = (EditText) findViewById(R.id.peso2);
+        peso3 = (EditText) findViewById(R.id.peso3);
+        String n1 = this.nota1.getText().toString();
+        String n2 = this.nota2.getText().toString();
+        String n3 = this.nota3.getText().toString();
+        String p1 = this.peso1.getText().toString();
+        String p2 = this.peso2.getText().toString();
+        String p3 = this.peso3.getText().toString();
+        result.putExtra("nota1",n1);
+        result.putExtra("nota2",n2);
+        result.putExtra("nota3",n3);
+        result.putExtra("peso1",p1);
+        result.putExtra("peso2",p2);
+        result.putExtra("peso3",p3);
+        startActivity(result);
+    }
+}
